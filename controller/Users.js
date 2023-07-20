1c31b8f9a6469797e41046e4d70b93c84722235c
shittu adewale <adewale.shittu@milsat.tech>
2023-07-20 08:18:50 +0100

    fixed sign up

--- controller/Users.js
+++ controller/Users.js
@@ -1,42 +1,78 @@
-require('dotenv').config()
-const User = require("../model/User")
-const {StatusCodes} = require('http-status-codes')
+require("dotenv").config();
+const {UnauthenticatedError, BadRequestError} = require('../errors') 
+const User = require("../model/User");
+const { StatusCodes } = require("http-status-codes");
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "zwingerfx@gmail.com",
+    pass: "gpvgnuuqnvnsngyy",
+  },
+});
 
-const getAllUsers = async (req, res) =>{
-    const users = await User.find({})
-    res.status(200).json(users)
+const getAllUsers = async (req, res) => {
+  const users = await User.find({});
+  res.status(200).json(users);
+};
+
+const createUser = async (req, res) => {
+  const { email, firstname, lastname } = req.body;
+  const userExists = await User.findOne({email})
+  if(userExists ){
+    throw new BadRequestError("User already exists")
 }
+  // const user = await User.create({
+  //   email: email,
+  //   firstname: firstname,
+  //   lastname: lastname,
+  // });
 
+  const user = await User.create({...req.body})
+   
+    res.status(StatusCodes.CREATED).json({user: {user}})
+};
+const submitInformation = async (req, res) => {
+  const email = req.params.email;
+  const currentUser = await User.findOneAndUpdate(
+    { email: email },
+    { ...req.body },
+    { returnOriginal: false }
+  );
+  if (currentUser) {
+    const mailOptions = {
+      from: "sheriffhardewale@gmail.com",
+      to: "sheriffhardewale@gmail.com",
+      subject: "New Account",
+      // text: 'That was easy!',
+      html: `<h1>Someone just submitted  in with the following info ${currentUser}, please visit the admin page to verify their document `,
+    };
+    res.status(200).json({ msg: "done" });
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+      }
+    });
+  } else {
+    res.status(404).json({
+      msg: "User not found",
+    });
 
-const createUser = async(req, res) =>{
-    const {email, firstname, lastname} = req.body
+    res.status(200).json(currentUser);
+  }
+};
 
-    const user = await User.create(
-        {"email": email,"firstname": firstname , "lastname": lastname }
-    )
-
-    res.status(201).json({
-        payload: user,
-        message:"user created successfully"
-    })
-
-
-}
-const submitInformation = async (req, res) =>{
-    const email = req.params.email
-    const currentUser = await User.findOneAndUpdate({email: email}, {...req.body}, {returnOriginal: false})
-    if(currentUser){
-        res.status(200).json(currentUser)
-
-    }else{
-        res.status(404).json({
-            msg: 'User not found'
-        })
-    }
-
+const getSingleUser = async (req, res) =>{
+  const userid = req.params.id
+  const currentUser = await User.findById(userid)
+  res.json(currentUser)
 }
 
 module.exports = {
-    getAllUsers, createUser, submitInformation
-}
+  getAllUsers,
+  createUser,
+  submitInformation,
+
+};
