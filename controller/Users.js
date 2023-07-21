fc57f0750e37a69edf76acf21ed30d2db4f2bb03
shittu adewale <adewale.shittu@milsat.tech>
2023-07-21 10:05:19 +0100

    added update user infomation

--- controller/Users.js
+++ controller/Users.js
@@ -22,12 +22,10 @@ const createUser = async (req, res) => {
   const { email } = req.body;
   const userExists = await User.findOne({ email });
   if (userExists) {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({
-        payload: "user already exists",
-        status: StatusCodes.BAD_REQUEST,
-      });
+    res.status(StatusCodes.BAD_REQUEST).json({
+      payload: "user already exists",
+      status: StatusCodes.BAD_REQUEST,
+    });
   }
   const user = await User.create({ ...req.body });
 
@@ -39,20 +37,22 @@ const submitInformation = async (req, res) => {
   const email = req.params.email;
   const { images } = req.body;
 
-  const uploadedImgs = images?.map(async image=>{
-    const upload =  await cloudinary.uploader.upload(image,
-         { 
-           upload_preset: 'unsigned_upload',
-           allowed_formats : ['png', 'jpg', 'jpeg', 'svg', 'ico', 'jfif', 'webp'],
-       }, 
-         function(error, result) {
-             if(error){
-                 console.log(error)
-             }
-              });
-     return upload
-   })
- 
+  const uploadedImgs = images?.map(async (image) => {
+    const upload = await cloudinary.uploader.upload(
+      image,
+      {
+        upload_preset: "unsigned_upload",
+        allowed_formats: ["png", "jpg", "jpeg", "svg", "ico", "jfif", "webp"],
+      },
+      function (error, result) {
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+    return upload;
+  });
+
   try {
     const fulfilled = await Promise.all(uploadedImgs).then((values) => {
       return values;
@@ -65,7 +65,7 @@ const submitInformation = async (req, res) => {
       { ...req.body, document: publicIds },
       { returnOriginal: false }
     );
-    
+
     if (currentUser) {
       const mailOptions = {
         from: "olaegbejoe@gmail.com",
@@ -85,7 +85,7 @@ const submitInformation = async (req, res) => {
   
         please visit the admin page to verify their document `,
       };
-     
+
       res.status(200).json({ msg: "done", payload: currentUser });
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
@@ -103,6 +103,24 @@ const submitInformation = async (req, res) => {
   }
 };
 
+const updatePaymentStatus = async (req, res) => {
+  const email = req.params.email;
+  try {
+    const currentUser = await User.findOneAndUpdate(
+      { email: email },
+      { ...req.body },
+      { returnOriginal: false }
+    );
+
+    if (currentUser) {
+      res.status(200).json({ msg: "done", payload: currentUser });
+    }else{
+      res.status(404).json({msg: "user not found"})
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 const getSingleUser = async (req, res) => {
   const userid = req.params.id;
   const currentUser = await User.findById(userid);
@@ -111,6 +129,8 @@ const getSingleUser = async (req, res) => {
 
 module.exports = {
   getAllUsers,
+  getSingleUser,
   createUser,
   submitInformation,
+  updatePaymentStatus,
 };
--- routes/Users.js
+++ routes/Users.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    getAllUsers, createUser, submitInformation
+    getAllUsers, createUser, submitInformation,
+  updatePaymentStatus
+
 } = require('../controller/Users')
 
 router.route('/all').get(getAllUsers)
 router.route('/create').post(createUser)
 router.route('/update/:email').put(submitInformation)
+router.route('/updatepayment/:email').put(updatePaymentStatus)
+
 
 
 module.exports = router
